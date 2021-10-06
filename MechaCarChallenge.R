6e8b6ba81a6d4f580a591c306a3fa85fa8779812
PGL50 <84110521+PGL50@users.noreply.github.com>
2021-10-05 20:24:27 -0500

    Deliverable 4

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,8 +5,6 @@ library(dplyr)
 #mecha_table <- read.csv(file=file1,check.names=F,stringsAsFactors = F)
 mecha_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
-
-
 #Multiple regression
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_table) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_table)) #generate summary statistics
